d5f8479bfb0654bc0750c381d891e3f10de3fde4
Oihane <dm3-1-05.17@uni.eus>
2018-04-04 23:07:11 +0200

    Erabiltzen ez diren import guztiak ezabatu

--- Proiektua_denda/src/proiektua_denda/Pertsona.java
+++ Proiektua_denda/src/proiektua_denda/Pertsona.java
@@ -10,14 +10,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
