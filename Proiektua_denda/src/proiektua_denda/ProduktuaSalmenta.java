6d1b960f11884a14dfa32964f957da1a5d0faf94
Oihane <dm3-1-05.17@uni.eus>
2018-04-12 10:14:20 +0200

    ProduktuaEskaera eta ProduktuaSalmenta klaseak sortu.

---
+++ Proiektua_denda/src/proiektua_denda/ProduktuaSalmenta.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proiektua_denda;
+
+import gestioa.Metodoak;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ProduktuaSalmenta {
+    /* ATRIBUTUAK */
+    private String prodKodea; 
+    private int kopurua;
+    private double prezioa;
+    
+    
+    /* GETTER and SETTER */
+    BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+    public String getProdKodea() {
+        return prodKodea;
+    }
+
+    public void setProdKodea(String prodKodea) {
+        this.prodKodea = prodKodea;
+    }
+
+    public int getKopurua() {
+        return kopurua;
+    }
+
+    public void setKopurua() {
+        try {
+            System.out.print("Sartu kopurua: ");
+            this.kopurua = Integer.parseInt(br.readLine());
+        }
+        catch (IOException gaizki) {
+            System.out.println(Metodoak.printGorriz("Arazoak daude datuak sartzerakoan."));
+        }
+        catch (NumberFormatException datuOkerrak) {
+            System.out.println(Metodoak.printGorriz("\tZenbaki bat sartu behar zenuen."));
+            setKopurua();
+        }
+    }
+
+    public double getPrezioa() {
+        return prezioa;
+    }
+
+    public void setPrezioa() {
+        try {
+            System.out.print("Sartu prezioa: ");
+            this.kopurua = Integer.parseInt(br.readLine());
+        }
+        catch (IOException gaizki) {
+            System.out.println(Metodoak.printGorriz("Arazoak daude datuak sartzerakoan."));
+        }
+        catch (NumberFormatException datuOkerrak) {
+            System.out.println(Metodoak.printGorriz("\tZenbaki bat sartu behar zenuen."));
+            setKopurua();
+        }
+    }
+    
+    
+}
