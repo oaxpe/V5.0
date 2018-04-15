8db1072c975bee38bc7287f266054f71e5628d7e
Oihane <dm3-1-05.17@uni.eus>
2018-04-15 16:43:32 +0200

    Erabiltzen ez diren import guztiak ezabatu

--- Proiektua_denda/src/proiektua_denda/Eskaera.java
+++ Proiektua_denda/src/proiektua_denda/Eskaera.java
@@ -10,11 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 /**
